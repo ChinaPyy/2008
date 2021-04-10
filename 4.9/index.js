a94c6a025945b944665a01ff3abbe5d8f0f3b935
unknown <771533260@qq.com>
2021-04-10 09:24:17 +0800

    Vue

---
+++ 4.9/index.js
@@ -0,0 +1,16 @@
+
+// var request = require('request');
+// console.log(request);
+// request('http://vue.api.comcto.com/api/helloworld.php', function (error, response, body) {
+//     if (!error && response.statusCode === 200) {
+//         console.log(body) // 请求成功的处理逻辑
+//     }
+// });
+
+//  引入数据方法
+// const axios = require("axios");
+// console.log(axios);
+// axios.get('http://vue.api.comcto.com/api/helloworld.php')
+//     .then(response => {
+//         console.log(response.data);
+//     })
